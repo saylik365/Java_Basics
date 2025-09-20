3929dbb002f1f4035a7927e6ceec5cd5e4d577bb
saylik365 <saylikulkarni0645@gmail.com>
2025-09-20 23:21:13 +0530

    commit

---
+++ Operators/logical.java
@@ -0,0 +1,11 @@
+public class logical {
+    public static void main(String[] args) {
+        boolean a = false;
+        boolean b = true;
+
+        System.out.println(a && b);
+        System.out.println(a || b);
+        System.out.println(!a);
+        System.out.println(!b);
+    }
+}
---
+++ Operators/relational.java
@@ -0,0 +1,13 @@
+public class relational {
+    public static void main(String[] args) {
+         int a  = 10;
+         int b = 12;
+
+         System.out.println(a == b); //false
+         System.out.println(a != b); //true
+         System.out.println(a > b); //false
+         System.out.println(a < b); //true
+         System.out.println(a <= b); //true
+         System.out.println(a >= b); //false
+    }
+}
