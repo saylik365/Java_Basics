eb736179473d824911eaf1e04c772a7c40217bf6
saylik365 <saylikulkarni0645@gmail.com>
2025-09-20 19:21:27 +0530

    First Commit

---
+++ Operators/Arithmetic.java
@@ -0,0 +1,17 @@
+public class Arithmetic{
+    public static void main(String[] args) {
+        int a = 40;
+        int b = 20;
+        int addition  = a+ b;
+        int subtraction  = a- b;
+        int Multiplication = a* b;
+        int division  = a/ b; //gives quotient
+        int modulus  = a% b; //gives remainder
+
+        System.out.println("The addition of a and b is: "+ addition);
+        System.out.println("The subtraction of a and b is: "+ subtraction);
+        System.out.println("The multiplication of a and b is: "+ Multiplication);
+        System.out.println("The division of a and b is: "+ division);
+        System.out.println("The modulus of a and b is: "+ modulus);
+    }
+}
---
+++ Operators/assignment.java
@@ -0,0 +1,13 @@
+public class assignment {
+    public static void main(String[] args) {
+        int a = 12;
+        int b = 7;
+        
+        // a += b;
+        // a -= b;
+        // a *= b;
+        a /= b;
+        // a %= b;
+        System.out.println(a);
+    }
+}
