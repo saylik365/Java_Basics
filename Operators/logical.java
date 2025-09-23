ca24078f7a74bcb9f232a2e56337b731f072191f
saylik365 <saylikulkarni0645@gmail.com>
2025-09-23 19:23:16 +0530

    commit

--- Operators/logical.java
+++ Operators/logical.java
@@ -1,11 +1,30 @@
 public class logical {
     public static void main(String[] args) {
-        boolean a = false;
-        boolean b = true;
+       int time  = 20;
 
-        System.out.println(a && b);
-        System.out.println(a || b);
-        System.out.println(!a);
-        System.out.println(!b);
+    //    if(time >= 10){
+    //         // System.out.println("Office is open");
+    //         if (time <= 20) {
+    //             System.out.println("Office is open");
+    //         }else{
+    //             System.out.println("Office is Closed");
+    //         }
+    //    }else{
+    //     System.out.println("Office is close");
+    //    }
+
+       if(time >= 10 && time<=20){
+        System.out.println("The Office is Open");
+       }else{
+        System.out.println("The Office is closed");
+       }
+
+
+    //    if(time ==10 || time ==20)
+    //    {
+    //     System.out.println("The office is Open");
+    //    }else{
+    //     System.out.println("The office is closed");
+    //    }
     }
 }
