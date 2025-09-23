e3b91e26e6ac269b3ec86257a075a447c5bfc7a6
saylik365 <saylikulkarni0645@gmail.com>
2025-09-23 20:03:31 +0530

    commit

---
+++ Loops/loop1.java
@@ -0,0 +1,8 @@
+public class loop1 {
+    public static void main(String[] args) {
+        String word = "hello world";
+        for(int i = 1; i<=5;i++){
+            System.out.println(word);
+        }
+    }
+}
