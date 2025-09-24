6c531b19b13b8e4d1fb0edf344f358ff15781e66
saylik365 <saylikulkarni0645@gmail.com>
2025-09-24 14:10:40 +0530

    commit

--- Practice/Loop3.java
+++ Practice/Loop3.java
@@ -1,3 +1,4 @@
+//Print sum of first N natural numbers.
 import java.util.Scanner;
 public class Loop3 {
     public static void main(String[] args) {
@@ -9,5 +10,10 @@ public class Loop3 {
             sum = sum+i;
         }
         System.out.println("Sum of N Natural Numbers is: "+ sum);
+
+        //Print multiplication table of a number.
+        for(int i =1; i<=10; i++){
+            System.out.println("The table is: "+ i* num);
+        }
     }
 }
