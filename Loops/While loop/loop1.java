e7cc9311756e4dfb0f628d19a2420bda2a4492e1
saylik365 <saylikulkarni0645@gmail.com>
2025-10-09 23:18:01 +0530

    commit

---
+++ Loops/While loop/loop1.java
@@ -0,0 +1,8 @@
+//in while loops, you don't know how many times a loop is going to continue but you know the condition.
+//for ex: you don't know how many days you have to go to the school but you know you have to go until semester ends.
+
+public class loop1 {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ Loops/loop4.java
@@ -0,0 +1,5 @@
+public class loop4 {
+    public static void main(String[] args) {
+        
+    }
+}
